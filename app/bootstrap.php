47e9aa7ee1c77e1c0397968489bf0a3e46d594ee
toto <test@test.com>
2023-01-18 21:06:19 +0200

    bootstrap add global function url delegate to Route::url

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,10 @@
 
 include_once '..' . DIRECTORY_SEPARATOR . 'config.php';
 
+function url(string $controller = null,string $action = null){
+    return \app\core\Route::url($controller, $action);
+}
+
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $classFile = '..'.DIRECTORY_SEPARATOR.$class . '.php';
