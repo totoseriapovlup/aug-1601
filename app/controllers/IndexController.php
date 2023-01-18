948b2c006f5549ab76e45a8f2526015205d3bacb
toto <test@test.com>
2023-01-18 18:26:36 +0200

    IndexController

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\controllers;
+
+use app\core\AbstractController;
+
+class IndexController extends AbstractController
+{
+
+    public function index()
+    {
+        echo 'IndexController@index';
+    }
+}
