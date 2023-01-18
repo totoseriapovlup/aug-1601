6ab2a399bca0068ed782a527ac480ee42e7b7494
toto <test@test.com>
2023-01-18 20:56:05 +0200

    realisation class

--- app/core/View.php
+++ app/core/View.php
@@ -6,5 +6,37 @@ namespace app\core;
 
 class View
 {
+    const VIEWS_DIR = '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'views';
+
+    protected $template = 'main';
+
+    protected $page;
+
+    public function __construct(string $template = null)
+    {
+        if (!is_null($template)) {
+            $this->template = $template;
+        }
+    }
+
+    public function render(string $page, array $params = [])
+    {
+        $this->page = $page;
+        include_once $this->getTemplatePath();
+    }
+
+    /**
+     * return formatted path to templates directory
+     * @return string
+     */
+    private function getTemplatePath()
+    {
+        return self::VIEWS_DIR . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $this->template . '.php';
+    }
+
+    private function getPagePath()
+    {
+        return self::VIEWS_DIR . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR . $this->page . '.php';
+    }
 
 }
