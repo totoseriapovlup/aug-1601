29eee7bc4daa7f87f23db848e921bbfb1ab07427
toto <test@test.com>
2023-01-23 18:47:35 +0200

    TaskController add store

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -4,6 +4,8 @@
 namespace app\controllers;
 
 
+use app\core\Route;
+use app\core\Validator;
 use app\models\TaskModel;
 
 class TaskController extends \app\core\AbstractController
@@ -25,7 +27,23 @@ class TaskController extends \app\core\AbstractController
         );
     }
 
-    public function create(){
+    public function create()
+    {
         $this->view->render('task_create');
+    }
+
+    public function store()
+    {
+        $name = filter_input(INPUT_POST, 'name');//TODO nobody has answer: how working filter_input
+        $validator = new Validator();
+        if (!$validator->validateName($name)) {
+            //TODO send errors
+            //TODO send old values
+            Route::redirect(Route::url('task','create'));
+        }
+        $this->model->add([
+            'name' => $name,
+        ]);
+        Route::redirect(Route::url('task','index'));
     }
 }
